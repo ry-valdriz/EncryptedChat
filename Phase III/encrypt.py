35d64f128a79a5d6a04a04c98d16926e11bc6423
Ryan Valdriz <31788821+ry101@users.noreply.github.com>
2018-10-14 22:10:31 -0700

    encrypted ciphertext with AES

--- Phase III/encrypt.py
+++ Phase III/encrypt.py
@@ -1,8 +1,11 @@
 # import cryptography
 # from cryptography import Fernet
-import Crypto
+import Crypto, json
+from base64 import b64encode
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
+from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes
 
 publicKey = RSA.generate(2048) #generate rsa key object
 
@@ -14,22 +17,34 @@ publicKeyFile = open(publicKeyAddress, 'rb')
 
 #print('contents of public key: ' + '\n', publicKeyFile.read())
 
-publicKey = RSA.import_key(publicKeyFile.read())
-
-cipher_rsa = PKCS1_OAEP.new(publicKey)
+publicKey = RSA.import_key(publicKeyFile.read()) #create rsa public key object
 
 message = input('Please enter message to encrypt:')
 
-cipherText = cipher_rsa.encrypt(message)
+message = message.encode('utf-8')
 
-#print('Encrypted Message: ', cipherText )
+cipher_rsa = PKCS1_OAEP.new(publicKey) #create rsa cipher object
 
-privateKeyAddress = input('Please enter the address of the private key: ')
+key_AES = get_random_bytes(32) #generate 256 bit key for AES
 
-privateKey = RSA.import_key(privateKeyAddress.read())
+cipher_AES = AES.new(key_AES, AES.MODE_CBC) #initialize AES object
 
-cipher_rsa = PKCS1_OAEP.new(privateKey)
+cipherTextBytes = cipher_AES.encrypt(pad(message, 128)) 
 
-print(cipher_rsa.decrypt(cipherText))
+iv = b64encode(cipher_AES.iv).decode('utf-8') 
+
+cipherText = b64encode(cipherTextBytes).decode('utf-8')
+
+output = json.dumps({'iv' : iv, 'ciphertext' : cipherText}) #encrypted message
+
+print(output)
+
+
+
+
+
+
+
+
 
 
