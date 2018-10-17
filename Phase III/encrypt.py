2c13a6f476a96ec3bcbadda5b664b3807d071829
ry101 <31788821+ry101@users.noreply.github.com>
2018-10-17 01:00:31 -0700

    new commit

--- Phase III/encrypt.py
+++ Phase III/encrypt.py
@@ -1,10 +1,10 @@
 # import cryptography
 # from cryptography import Fernet
 import Crypto, json
-from base64 import b64encode
+from base64 import b64encode, b64decode
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad,unpad
 from Crypto.Random import get_random_bytes
 from Crypto.Hash import HMAC, SHA256
 
@@ -31,21 +31,23 @@ def encryptMessage(publicKeyAddress, message): #encryption function
     cipherText_Message_Bytes = cipher_AES.encrypt(pad(message, 128)) #encrypt message
 
     key_HMAC = get_random_bytes(32) 
-
+    print('key_hmac: ',b64encode(key_HMAC).decode('utf-8'))
     h = HMAC.new(key_HMAC, digestmod=SHA256) #instantiate hmac object
 
-    h.update(message) #authenticate the message
-
-    print('HMAC tag: ', h.hexdigest()) #print out hmac tag of the authenticated message
+    print('print cipher text in bytes: ', cipherText_Message_Bytes)
+   
 
     # keys_bytearray = bytearray(key_AES) #copy aes key to bytearray
     # keys_bytearray.append(key_HMAC) #append hmac key to keys_bytearray
     ka = b64encode(key_AES).decode('utf-8') #string of aes key
-    print('AES Key: ', ka)
+    
+    print('AES key length: ', len(ka))
     kh = b64encode(key_HMAC).decode('utf-8') #string of hmac key
-    print('HMAC Key: ',kh)
+    print('key_hmac2: ', kh)
+    print('hmac key length: ', len(kh))
     k = ka + kh #concatenate the two keys
     print('Concatenated Keys:', k) #test
+    print('concatenated keys length:', len(k))
 
     keys_bytes = k.encode('utf-8') #convert back to bytes
 
@@ -54,12 +56,21 @@ def encryptMessage(publicKeyAddress, message): #encryption function
 
     print('Encrypted keys:' , cipherText_Keys_String ) #print out AES and HMAC keys concatenated and encrypted
 
-    #iv = b64encode(cipher_AES.iv).decode('utf-8') #conver iv from bytes to string
+    iv = b64encode(cipher_AES.iv).decode('utf-8') #conver iv from bytes to string
+    print('iv : ', iv)
 
     cipherText = b64encode(cipherText_Message_Bytes).decode('utf-8') #convert cipher text from bytes to string
+    h.update(cipherText.encode('utf-8')) #authenticate the message
 
+    print('HMAC tag: ', h.hexdigest()) #print out hmac tag of the authenticated message
+
+    print('hmac tag during encryption:', h.hexdigest())
+    print('ciphertext during encryption: ', cipherText)
     JSON_output = json.dumps({'AES_ciphertext' : cipherText, 'RSA_ciphertext' : cipherText_Keys_String, 'HMAC_Tag' : h.hexdigest() }) 
 
+    print('hmac tag  encryption: ', h.hexdigest())
+    print('AES Key: ', ka)
+    print('HMAC Key: ',kh)
     #print(JSON_output)
     outfile = open("Testing.encrypt", "w")
     outfile.write(JSON_output)
@@ -70,22 +81,57 @@ def encryptMessage(publicKeyAddress, message): #encryption function
 
 def decryptMessage(privateKeyAddress, JSON_output):
     
-    privateKey = RSA.generate(2048) #generate rsa key object
     jsonFile = json.loads(JSON_output)
     AES_ciphertext = jsonFile['AES_ciphertext']
-    RSA_ciphertext = jsonFile['RSA_ciphertext']
+    #AES_ciphertext = b64decode(jsonFile['AES_ciphertext']) #need it in bytes for HMAC
+    #RSA_ciphertext = jsonFile['RSA_ciphertext']
+    RSA_ciphertext = b64decode(jsonFile['RSA_ciphertext']) #encrypted keys
     HMAC_tag = jsonFile['HMAC_Tag']
+    print('hmac_tag_encryption in decryption: ', HMAC_tag)
 
+    privateKey = RSA.generate(2048) #generate rsa key object
     privateKeyFile = open(privateKeyAddress, 'rb')
     privateKey = RSA.import_key(privateKeyFile.read()) #create rsa private key object
 
-    cipher_rsa = PKCS1_OAEP.new(privateKey) #create rsa cipher object
-    keys_plaintext = cipher_rsa.decrypt(RSA_ciphertext)
-    AES_key = keys_plaintext[0:255]
-    print(AES_key)
+    print('AES ciphertext during decryption', AES_ciphertext)
 
+    cipher_rsa = PKCS1_OAEP.new(privateKey) #create rsa cipher object
+    
+    keys_plaintext = cipher_rsa.decrypt(RSA_ciphertext)
+    
+    keys_plaintext.decode('utf-8')
+    
+    print('RSA_ciphertext length decryption: ', len(RSA_ciphertext))
+    AES_key = keys_plaintext[0:43]
+    HMAC_key = keys_plaintext[44:87]
+
+    print('aes key during decryption', AES_key)
+    print('HMAC key during decryption', HMAC_key)
+    #print('AES_Key: ', AES_key)
     #print(AES_ciphertext)
 
+    #HMAC_key.decode('utf-8') #change to byte string for HMAC
+    hm = HMAC.new(HMAC_key, digestmod=SHA256) #instantiate hmac object
+    
+    hm.update(AES_ciphertext.encode('utf-8'))
+
+
+    print('hmac tag during decryption', hm.hexdigest())
+
+    # if hm.verify(HMAC_tag.encode('utf-8')):
+    #     print('mac tag is authentic')
+    # else:
+    #     print('message or key is wrong')
+    
+    try:
+        hm.verify(HMAC_tag.encode('utf-8'))
+        print('the message is authentic')
+    except ValueError:
+        print('The message or key is wrong')
+    
+
+    
+    
 
     return
 
