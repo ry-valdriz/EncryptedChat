821ba60009ede076d73dd5ce3bfca8de633d01c2
Ryan Valdriz <31788821+ry101@users.noreply.github.com>
2018-12-09 23:44:40 -0800

    renamed phase III

--- phase4/app.js
+++
@@ -1,6 +0,0 @@
-//app.js
-var express = require('express');
-var app = express();
-var db = require('./db'); 
-
-module.exports = app;
--- phase4/db.js
+++
@@ -1,4 +0,0 @@
-//db.js
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://mongodb://<duodolo>:<cecs478>@ds147011.mlab.com:47011/duodolo');
