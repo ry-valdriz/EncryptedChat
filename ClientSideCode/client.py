c91c232652b823bd3d0afb8ac05815e03ca3d314
Ryan Valdriz <31788821+ry101@users.noreply.github.com>
2018-12-11 00:40:35 -0800

    client app

--- ClientSideCode/client.py
+++ ClientSideCode/client.py
@@ -2,27 +2,6 @@ import requests
 import json
 from encrypt_decrypt import encryptMessage, decryptMessage
 
-#def main()
-    # print("----------------------")
-    # print("DuoDolo encrypted chat")
-    # print("----------------------" + "\n")
-
-    # while(True): #reiterate the app until the user decides to exit
-    #     print("1. Register")
-    #     print("2. Login")
-    #     print("3. Quit")
-    #     choice = input("Select a number: ")
-    #     print("")
-    #     if(choice == 1):
-    #         Register()
-    #     elif(choice == 2):
-    #         Login()
-    #     else:
-    #         print("---------------")
-    #         print("Exiting DuoDolo")
-    #         print("---------------")
-    #         break
-
 def Login():
     #https://www.pythonforbeginners.com/requests/using-requests-in-python
     url = 'http://localhost:3000/api/auth/login'
@@ -77,27 +56,75 @@ def Register():
         return
     
 def chat(jwt):
+    send_url = 'http://localhost:3000/api/Message/send'
+    receive_url = 'http://localhost:3000/api/Message/receive'
+    
+    while(1):
+        print("\n" + "--------------------" + "\n"+  "CHAT" + "\n" + "--------------------")
+        print("1. Send")
+        print("2. Receive")
+        print("3. Exit")
+        choice = input("Please select a number: ")
+
+        if(choice == '1'): //send
+            recipient = input("Please enter email of recipient: ")
+            message = input("message: ")
+            publicKeyAddress = input("Please enter address of public key: ")
+            
+            cipherText = encryptMessage(publicKeyAddress, message)
+            payload = {'recipient' : recipient, 'content' : cipherText}
+            headers = {'x-access-token' : jwt}
+
+            r = requests.post(send_url, data = payload, headers = headers)
+            if(r.status_code == 500):
+                print("Error sending the message. . .")
+            elif(r.status_code == 200):
+                print("Message sent . . . ")
+            else:
+                print("Something went wrong. . . ")
+
+        elif(choice == '2'): //receive
+            headers = {'x-access-token' : jwt}
+            r = requests.get(receive_url, headers = headers)
+            json_response = r.json()
+            privateKeyAddress = input("Please enter the address of your private key: ")
+            //iterate through messages
+            for x in json_response:
+                sender = x['sender']
+                recipient = x['recipient']
+                ciphertext = x['content']
+
+                plainText = decryptMessage(privateKeyAddress,json_response[x])
+
+                print("Sender: ", sender)
+                print("Recipient: ", recipient)
+                print("Message" , plainText)
+                print("-----------------------------------------")
+                print("")
+        else:
+            print("Exiting. . . ." + "\n")
+
     return
 
 
-#MAIN FUNCTION
-print("----------------------")
-print("DuoDolo encrypted chat")
-print("----------------------" + "\n")
+def main():
+    print("----------------------")
+    print("DuoDolo encrypted chat")
+    print("----------------------" + "\n")
 
-while(True): #reiterate the app until the user decides to exit
-    print("1. Register")
-    print("2. Login")
-    print("3. Quit")
-    choice = input("Select a number: ")
-    print("")
-    if(choice == '1'):
-        Register()
-    elif(choice == '2'):
-        Login()
-    else:#exit
-        print("---------------")
-        print("Exiting DuoDolo")
-        print("---------------")
-        break
+    while(True): #reiterate the app until the user decides to exit
+        print("1. Register")
+        print("2. Login")
+        print("3. Quit")
+        choice = input("Select a number: ")
+        print("")
+        if(choice == '1'):
+            Register()
+        elif(choice == '2'):
+            Login()
+        else:#exit
+            print("---------------")
+            print("Exiting DuoDolo")
+            print("---------------")
+            break
 
